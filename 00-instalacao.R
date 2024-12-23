55c996fd37120bff049926499261da0f25980346
arturdamiao <arturdamiao@hotmail.com.br>
2024-12-23 17:48:00 -0300

    Commit incial com mudanças no currículo. Traduções para o PT-BR principalmente

---
+++ 00-instalacao.R
@@ -0,0 +1,12 @@
+# install.packages("devtools")
+devtools::install_github("nstrayer/datadrivencv")
+
+
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Artur Damião",
+  data_location = "https://docs.google.com/spreadsheets/d/1VaimCwoH2ZyNakthHAi0xzLqurQhsTSDnok-VXhNE1w/edit?usp=sharing",
+  source_location = "https://github.com/nstrayer/cv"
+)
+
